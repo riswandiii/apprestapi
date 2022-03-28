b9be2685d6ef1014b5e8ee52cffd8c19d7938dc7
riswandiii <riswandiandi017@gmail.com>
2022-03-28 21:36:23 +0700

    menambha file koneksi

---
+++ koneksi.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbrestapi'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('mysql terkoneksi');
+});
